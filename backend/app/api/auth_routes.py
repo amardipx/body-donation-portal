58a1837f4c97023287c77b22c9f6fa481e5243b3
Amardip Karn <karnamardip@gmail.com>
2026-06-18 14:52:30 +0530

    finish user signup along with password hashing

--- backend/app/api/auth_routes.py
+++ backend/app/api/auth_routes.py
@@ -4,6 +4,7 @@ from sqlalchemy.orm import Session
 from app.schemas.user_schema import UserSignup
 from app.db.database import get_db
 from app.db.models import User
+from app.utils.security import hash_password
 
 router = APIRouter(
     prefix="/auth",
@@ -16,7 +17,7 @@ def signup(user: UserSignup, db: Session = Depends(get_db)):
         full_name=user.full_name,
         email=user.email,
         phone=user.phone,
-        password_hash=user.password,
+        password_hash=hash_password(user.password),
         role="donor",
     )
 
