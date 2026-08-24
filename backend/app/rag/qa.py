6a68f827d564388262b593f3015d39b3fce5af44
Vivek Mandal <vivek171004@gmail.com>
2026-08-25 00:08:38 +0530

    changed rag qa model

--- backend/app/rag/qa.py
+++ backend/app/rag/qa.py
@@ -16,7 +16,7 @@ _embeddings = GoogleGenerativeAIEmbeddings(
     )
 
 _llm = ChatGroq(
-    model="llama-3.1-8b-instant",
+    model="openai/gpt-oss-20b",
     temperature=0,
     max_retries=2,
     api_key=os.getenv("GROQ_API_KEY"),
